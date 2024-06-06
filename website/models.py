e32041228b1b6c2a6eb844318b9278082a238c47
ZachLyons <zlyons70@gmail.com>
2024-06-06 18:52:50 -0400

    Setup Database structure

--- website/models.py
+++ website/models.py
@@ -1,8 +1,27 @@
 '''This file is used to handle the database of the website'''
 from . import db
 from flask_login import UserMixin
+from sqlalchemy.sql import func
 
+class Earning(db.Model):
+    '''This class tells the Database that all objects must have this structure'''
+    id = db.Column(db.Integer, primary_key=True)
+    # the below data that our user will be storing is going to be the tip number
+    data = db.Column(db.Integer(1000))
+    # this automatically sets the date to the current date and time
+    #TODO Will need to change in the future
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
+    # This is a foreign key that is linked to the user table
+    # this is used to link the note to the user
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
 class User(db.Model, UserMixin):
     '''This class is used to define the user table in the database'''
     # Primary key is a unique identifier for each user
     id = db.Column(db.Integer, primary_key=True)
+    # max string lenght is 150 and each email must be unique
+    email = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    username = db.Column(db.String(150), unique=True)
+    # below is essentially a list of all notes the user has created
+    earning = db.relationship('Earning')
